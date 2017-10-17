f7e85ab7503a26acfad101601654221a836b0174
ARCDS\ifajardo <ifajardo@arrccorp.com>
2017-10-17 13:56:10 -0400

    add tc-hl and sass functions

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,54 @@
  const path = require('path');
+ const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-  module.exports = {
-    entry: './src/arctravelconnect.js',
-    output: {
-      filename: 'arctravelconnect.min.js',
-      path: path.resolve(__dirname, 'dist')
-    },
-		module: {
-			rules: [
-				{
-					test: /\.js$/,
-					exclude: /(node_modules|bower_components)/,
-					use: {
-						loader: 'babel-loader',
-						options: {
-							presets: ['env']
+ const extractSass = new ExtractTextPlugin({
+    filename: "[name].min.css",
+    disable: process.env.NODE_ENV === "development"
+});
+ 
+  module.exports = [
+		{
+			entry: {
+					tc: './src/arctravelconnect.js',
+					tc_hl: './src/arctravelconnect-highlights.js'
+			},
+			output: {
+				filename: '[name].min.js',
+				path: path.resolve(__dirname, 'dist')
+			},
+			module: {
+				rules: [
+					{
+						test: /\.js$/,
+						exclude: /(node_modules|bower_components)/,
+						use: {
+							loader: 'babel-loader',
+							options: {
+								presets: ['env']
+							}
 						}
+					},
+					{
+						test: /\.scss$/,
+						use: extractSass.extract({
+								use: [{
+										loader: "css-loader"
+								}, {
+										loader: "sass-loader"
+								}],
+								fallback: "style-loader"
+						})
 					}
+				]
+			},
+			resolve: {
+				alias: {
+					'masonry': 'masonry-layout',
+					'isotope': 'isotope-layout'
 				}
+			},
+			plugins: [
+        extractSass
 			]
-		},
-		resolve: {
-			alias: {
-				'masonry': 'masonry-layout',
-				'isotope': 'isotope-layout'
-			}
 		}
-  };
+  ];
