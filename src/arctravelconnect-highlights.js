0fb5a87801d61c3c127f78bd1a03c2f0cacf0b68
ianfajardo <ianfajardozx@gmail.com>
2017-10-20 12:42:22 -0400

    highlight html update

--- src/arctravelconnect-highlights.js
+++ src/arctravelconnect-highlights.js
@@ -2,6 +2,6 @@ import './tc_hl.scss';
 
 $(document).ready(function(){
 
-	$(".jumbotron .foreground").prepend('<div class="custom-brow"><a href="http://arctravelconnect.com" class="logo travelconnectlogo icon-travelconnect-logo" title="logo"><span>Highlights</span></a></div>');
+	$(".jumbotron .foreground").prepend('<div class="custom-brow"><a href="http://arctravelconnect.com/thank-you?link=highlights" class="logo travelconnectlogo icon-travelconnect-logo" title="logo"><span>Highlights</span></a></div>');
 
 });
