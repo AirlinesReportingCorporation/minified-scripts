5a3fdc1c37b9667d243dd1f849da963a413c9bfa
ianfajardo <ianfajardozx@gmail.com>
2018-01-31 13:35:28 -0500

    tc-video height 100%

--- src/arctravelconnect.js
+++ src/arctravelconnect.js
@@ -100,7 +100,7 @@ var Isotope = require('isotope-layout');
 
 		var g = "<div class='submission'><h3>You are now connected! <h3/>We’ll keep you informed on the latest TravelConnect 2018 updates.</div>";
 		
-		var tcVid = "<iframe width='560' height='315' src='https://www.youtube.com/embed/j_JHHI-KRpE?rel=0&amp;showinfo=0&autoplay=1' frameborder='0' allow='autoplay; encrypted-media' allowfullscreen></iframe>"
+		var tcVid = "<iframe width='' height='100%' src='https://www.youtube.com/embed/j_JHHI-KRpE?rel=0&amp;showinfo=0&autoplay=1' frameborder='0' allow='autoplay; encrypted-media' allowfullscreen></iframe>"
 
 		e = b + e;
 		f = a + f;
